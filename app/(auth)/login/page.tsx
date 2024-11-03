4d1007b0ab7b424d51e1acaabba29956d4ccb6cf
Om Kute <kuteom2006@gmail.com>
2024-11-03 11:59:33 +0530

    Added dark Mode and Login Card

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,11 @@
+import { LoginForm } from "@/components/login-form"
+
+const Page = () => {
+  return (
+    <div className="flex h-screen w-full items-center justify-center px-4">
+      <LoginForm />
+    </div>
+  )
+}
+
+export default Page;
---
+++ app/(auth)/signup/page.tsx
@@ -0,0 +1,7 @@
+export default function LoginPage() {
+    return (
+        <div>
+            Hi there from signup!
+        </div>
+    )
+}
